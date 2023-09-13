73b1ad3d12ff633dbfe2f8428bebebcded449dc0
Eren <86615310+erent8@users.noreply.github.com>
2023-09-13 13:20:32 +0300

    Create output_values.java

---
+++ output_values.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Merhaba Java dünyası!");
+        System.out.println("Deneme yazıları");
+        System.out.println("I am learning Java.");
+        System.out.println("başaracağım!");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
+    // Yorum satırları bu şekilde...
+
+    /* Yorum satırınız birkaç satırdan oluşuyorsa bunu
+    kullanmalısınız. */
+
+        String name = "Eren";
+        System.out.println(name);
+        int myNum = 10;
+        System.out.println(myNum);
+        // Değeri atamadan bir değişken bildirebilir ve değeri daha sonra atayabilirsiniz
+        int myNumber;
+        myNumber = 20;
+        System.out.println(myNumber);
+
+        int myNumber1 = 15;
+        myNumber1 = 20;  // myNum is now 20
+        System.out.println(myNumber1);
+
+
+        //int myNumber2 = 5;
+        //float myFloatNum = 5.99f;
+        //char myLetter = 'D';
+        //boolean myBool = true;
+        //String myText = "Hello";
+        
+    }
+}
