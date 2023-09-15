ebe7438dbc05b9fc28ff01ce2288dd9791f41d3c
Eren <86615310+erent8@users.noreply.github.com>
2023-09-15 14:03:28 +0300

    Update data_types.java

--- data_types.java
+++ data_types.java
@@ -23,6 +23,78 @@ public class Main {
         char	2 bytes	Stores a single character/letter or ASCII values
         */
 
+        public class Main {
+    public static void main(String[] args) {
+
+        byte myNumber = 10;
+        System.out.println(myNumber);
+
+        short myNum = 5000;
+        System.out.println(myNum);
+
+        int myNumb = 100000;
+        System.out.println(myNumb);
+
+        long myNum1 = 15000000000L;
+        System.out.println(myNum1);
+
+        float myNum2 = 5.75f;
+        System.out.println(myNum2);
+
+        double myNum3 = 19.99d;
+        System.out.println(myNum3);
+
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+
+        char myGrade = 'B';
+        System.out.println(myGrade);
+
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+
+        String greeting = "Eren'in ders notları";
+        System.out.println(greeting);
+
+        // Java Non-Primitive Data Types //
+
+        /*
+        Non-Primitive Data Types
+        Non-primitive data types are called reference types because they refer to objects.
+        The main difference between primitive and non-primitive data types are:
+        Primitive types are predefined (already defined) in Java. Non-primitive types are created by the programmer and is not defined by Java (except for String).
+        Non-primitive types can be used to call methods to perform certain operations, while primitive types cannot.
+        A primitive type has always a value, while non-primitive types can be null.
+        A primitive type starts with a lowercase letter, while non-primitive types starts with an uppercase letter.
+        Examples of non-primitive types are Strings, Arrays, Classes, Interface, etc. You will learn more about these in a later chapter.
+
+        TURKISH LANG
+
+        İlkel Olmayan Veri Türleri
+        İlkel olmayan veri türlerine nesnelere atıfta bulundukları için referans türleri denir.
+
+        İlkel ve ilkel olmayan veri türleri arasındaki temel fark şunlardır:
+
+        İlkel türler Java'da önceden tanımlanmıştır (zaten tanımlanmıştır). İlkel olmayan türler programcı tarafından oluşturulur ve Java tarafından tanımlanmaz (String hariç).
+        İlkel olmayan türler, belirli işlemleri gerçekleştirecek yöntemleri çağırmak için kullanılabilirken, ilkel türler bunu yapamaz.
+        İlkel bir türün her zaman bir değeri vardır, ilkel olmayan türler ise boş olabilir.
+        İlkel türler küçük harfle başlarken, ilkel olmayan türler büyük harfle başlar.
+        İlkel olmayan türlere örnek olarak String'ler, Diziler, Sınıflar, Arayüz vb. gösterilebilir. Bunlar hakkında daha sonraki bir bölümde daha fazla bilgi edineceksiniz.
+        */
+    
+
+    }
+}
+
         int myNumb = 1;
         float myFloatnumber = 4.32f;
 
