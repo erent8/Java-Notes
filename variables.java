5e576382c2253136c6d5ed8a93ae04337b9a4e02
Eren <86615310+erent8@users.noreply.github.com>
2023-09-15 13:48:40 +0300

    Update variables.java

--- variables.java
+++ variables.java
@@ -3,28 +3,7 @@ public class Main {
         public class Main {
     public static void main(String[] args) {
 
-        // Data Types //
-
-        int myNum = 5;               // Integer (whole number)
-        float myFloatNum = 5.99f;    // Floating point number
-        char myLetter = 'D';         // Character
-        boolean myBool = true;       // Boolean
-        String myText = "Hello";     // String
-
-
-            // Primitive Data Types //
-
-        /*
-        byte	1 byte	Stores whole numbers from -128 to 127
-        short	2 bytes	Stores whole numbers from -32,768 to 32,767
-        int	4 bytes	Stores whole numbers from -2,147,483,648 to 2,147,483,647
-        long	8 bytes	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
-        float	4 bytes	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
-        double	8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits
-        boolean	1 bit	Stores true or false values
-        char	2 bytes	Stores a single character/letter or ASCII values
-        */
-
+        
         int myNumb = 1;
         float myFloatnumber = 4.32f;
 
