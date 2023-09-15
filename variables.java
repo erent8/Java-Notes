040c50dc625413de8223357f353050390b01ac9a
Eren <86615310+erent8@users.noreply.github.com>
2023-09-15 13:47:52 +0300

    Update variables.java

--- variables.java
+++ variables.java
@@ -1,28 +1,77 @@
 public class Main {
     public static void main(String[] args) {
-        String name = "Eren";
-        System.out.println("Hello " + name);
+        public class Main {
+    public static void main(String[] args) {
+
+        // Data Types //
+
+        int myNum = 5;               // Integer (whole number)
+        float myFloatNum = 5.99f;    // Floating point number
+        char myLetter = 'D';         // Character
+        boolean myBool = true;       // Boolean
+        String myText = "Hello";     // String
+
+
+            // Primitive Data Types //
+
+        /*
+        byte	1 byte	Stores whole numbers from -128 to 127
+        short	2 bytes	Stores whole numbers from -32,768 to 32,767
+        int	4 bytes	Stores whole numbers from -2,147,483,648 to 2,147,483,647
+        long	8 bytes	Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+        float	4 bytes	Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+        double	8 bytes	Stores fractional numbers. Sufficient for storing 15 decimal digits
+        boolean	1 bit	Stores true or false values
+        char	2 bytes	Stores a single character/letter or ASCII values
+        */
+
+        int myNumb = 1;
+        float myFloatnumber = 4.32f;
+
+            // Integer Types //
+
+        byte myNumber = 100;
+        System.out.println(myNumber);
+
+    // The short data type can store whole numbers from -32768 to 32767:
+        short myNumber1 = 5000;
+        System.out.println(myNumber1);
+
+
+        int myNumber2;
+        myNumber2 = 10;
+        myNumber2 = 129;
+        System.out.println(myNumber2); // The last data entered as output is received well.
+
+        int myNumber3 = 5;
+        float myFloatNumb = 5.99f;
+        char myLetter2 = 'D';
+        boolean myBoolean = true;
+        String myText1 = "Hello";
+        System.out.println(myText1);
+
         String firstname = "Eren";
-        String lastname = " Terzi";
-        String fullname = firstname + lastname;
-        System.out.println(fullname);
+        System.out.println(firstname);
 
-        int x = 521;
-        int y = 611;
-        System.out.println(x + y); // Print the value of x + y
+            // Display Variables //
 
-        int a = 53;
-        int b = 61;
-        int c = 50;
-        System.out.println(a + b + c);
+        String name  = "Eren";
+        System.out.println("Heelloo" + name);
 
-        int q = 5, w = 6, e = 50;
-        System.out.println(q + w + e);
+        String firstName = "Eren ";
+        String lastName = "Trz";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
 
-        int f, d, g;
-        f = d = g = 50;
-        System.out.println(f + d + g);
+        int x = 597;
+        int y = 632;
+        int z = 987;
 
+        System.out.println("Result: " + (x + y + z)); // Print the value of x + y
+
+        
+    }
+}
         /* Identifiers
         All Java variables must be identified with unique names.
 
