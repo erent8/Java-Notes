f813cd632f96cd518901a3f3f1aab9c5d337831d
Eren <86615310+erent8@users.noreply.github.com>
2023-09-16 14:55:32 +0300

    Create type_casting.java

---
+++ type_casting.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+            // Java Type Casting //
+
+        /* Widening Casting (automatically) - converting a smaller type to a larger type size
+        byte -> short -> char -> int -> long -> float -> double
+
+        Narrowing Casting (manually) - converting a larger type to a smaller size type
+        double -> float -> long -> int -> char -> short -> byte
+        */
+
+        int myInt = 9;
+        double myDouble = myInt; // Automatic casting: int to double
+
+        System.out.println(myInt);      // Outputs 9
+        System.out.println(myDouble);   // Outputs 9.0
+        double myDouble1 = 9.78d;
+        int myInt1 = (int) myDouble1; // Manual casting: double to int
+
+        System.out.println(myDouble);   // Outputs 9.78
+        System.out.println(myInt1);      // Outputs 9
+
+
+
+
+    }
+}
